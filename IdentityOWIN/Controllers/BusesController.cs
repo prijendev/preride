5c9dfecf1da399b2d1e96e2010d38c2f8d62c31e
STOL7 <prijenkhokhani58@gmail.com>
2019-11-11 13:52:42 +0530

    Update

---
+++ IdentityOWIN/Controllers/BusesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IdentityOWIN;
+
+namespace IdentityOWIN.Controllers
+{
+    public class BusesController : Controller
+    {
+        private Entities1 db = new Entities1();
+
+        // GET: Buses
+        public ActionResult Index()
+        {
+            return View(db.Buses.ToList());
+        }
+
+        // GET: Buses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bus bus = db.Buses.Find(id);
+            if (bus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bus);
+        }
+
+        // GET: Buses/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Buses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Arrival_time,Departure_time,From,To,Type")] Bus bus)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Buses.Add(bus);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bus);
+        }
+
+        // GET: Buses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bus bus = db.Buses.Find(id);
+            if (bus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bus);
+        }
+
+        // POST: Buses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Arrival_time,Departure_time,From,To,Type")] Bus bus)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bus).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bus);
+        }
+
+        // GET: Buses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bus bus = db.Buses.Find(id);
+            if (bus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bus);
+        }
+
+        // POST: Buses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bus bus = db.Buses.Find(id);
+            db.Buses.Remove(bus);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ IdentityOWIN/Controllers/TicketsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IdentityOWIN;
+
+namespace IdentityOWIN.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private Entities1 db = new Entities1();
+
+        // GET: Tickets
+        public ActionResult Index()
+        {
+            var tickets = db.Tickets.Include(t => t.AspNetUser).Include(t => t.Bus);
+            return View(tickets.ToList());
+        }
+
+        // GET: Tickets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        // GET: Tickets/Create
+        public ActionResult Create()
+        {
+            ViewBag.User_Id = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.Bus_Id = new SelectList(db.Buses, "Id", "From");
+            return View();
+        }
+
+        // POST: Tickets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,User_Id,Bus_Id,Date")] Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tickets.Add(ticket);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.User_Id = new SelectList(db.AspNetUsers, "Id", "Email", ticket.User_Id);
+            ViewBag.Bus_Id = new SelectList(db.Buses, "Id", "From", ticket.Bus_Id);
+            return View(ticket);
+        }
+
+        // GET: Tickets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.User_Id = new SelectList(db.AspNetUsers, "Id", "Email", ticket.User_Id);
+            ViewBag.Bus_Id = new SelectList(db.Buses, "Id", "From", ticket.Bus_Id);
+            return View(ticket);
+        }
+
+        // POST: Tickets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,User_Id,Bus_Id,Date")] Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ticket).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.User_Id = new SelectList(db.AspNetUsers, "Id", "Email", ticket.User_Id);
+            ViewBag.Bus_Id = new SelectList(db.Buses, "Id", "From", ticket.Bus_Id);
+            return View(ticket);
+        }
+
+        // GET: Tickets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        // POST: Tickets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ticket ticket = db.Tickets.Find(id);
+            db.Tickets.Remove(ticket);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
